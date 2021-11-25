ca2a3073e540c2a1ca850cb221f2fdf2d4749e49
Henry Gabryjelski <henrygab@users.noreply.github.com>
2021-11-25 13:52:24 -0800

    Indentation only.

--- esp8266-fastled-webserver/Fields.cpp
+++ esp8266-fastled-webserver/Fields.cpp
@@ -58,246 +58,246 @@ inline namespace InternalFieldsDetails {
   inline namespace LegacyStringGettersAndSetters { // This just helps folding / hiding these functions
 
 
-//String setPower(String value) {
-//  power = value.toInt();
-//  if(power < 0) power = 0;
-//  else if (power > 1) power = 1;
-//  return String(power);
-//}
+    //String setPower(String value) {
+    //  power = value.toInt();
+    //  if(power < 0) power = 0;
+    //  else if (power > 1) power = 1;
+    //  return String(power);
+    //}
 
-String getPower() {
-  return String(power);
-}
+    String getPower() {
+      return String(power);
+    }
 
-//String setBrightness(String value) {
-//  brightness = value.toInt();
-//  if(brightness < 0) brightness = 0;
-//  else if (brightness > 255) brightness = 255;
-//  return String(brightness);
-//}
+    //String setBrightness(String value) {
+    //  brightness = value.toInt();
+    //  if(brightness < 0) brightness = 0;
+    //  else if (brightness > 255) brightness = 255;
+    //  return String(brightness);
+    //}
 
-// TODO: wrapper class for EEPROM settings, to help ensure clear understanding
-//       of all locations that can cause settings committed / EEPROM write.
-//       Also simplifies reading prior value before writing new value (extend EEPROM life)
-//       Also simplifies later extending CRC, transactional updates, bypassing "dead"
-//       areas of EEPROM, etc.
+    // TODO: wrapper class for EEPROM settings, to help ensure clear understanding
+    //       of all locations that can cause settings committed / EEPROM write.
+    //       Also simplifies reading prior value before writing new value (extend EEPROM life)
+    //       Also simplifies later extending CRC, transactional updates, bypassing "dead"
+    //       areas of EEPROM, etc.
 
-String getBrightness() {
-  return String(brightness);
-}
+    String getBrightness() {
+      return String(brightness);
+    }
 
-String getPattern() {
-  return String(currentPatternIndex);
-}
+    String getPattern() {
+      return String(currentPatternIndex);
+    }
 
-String getPalette() {
-  return String(currentPaletteIndex);
-}
+    String getPalette() {
+      return String(currentPaletteIndex);
+    }
 
-String getAutoplay() {
-  return String(autoplay);
-}
+    String getAutoplay() {
+      return String(autoplay);
+    }
 
-String getAutoplayDuration() {
-  return String(autoplayDuration);
-}
+    String getAutoplayDuration() {
+      return String(autoplayDuration);
+    }
 
-String getShowClock() {
-  return String(showClock);
-}
+    String getShowClock() {
+      return String(showClock);
+    }
 
-String getClockBackgroundFade() {
-  return String(clockBackgroundFade);
-}
+    String getClockBackgroundFade() {
+      return String(clockBackgroundFade);
+    }
 
-String getSolidColor() {
-  return String(solidColor.r) + "," + String(solidColor.g) + "," + String(solidColor.b);
-}
+    String getSolidColor() {
+      return String(solidColor.r) + "," + String(solidColor.g) + "," + String(solidColor.b);
+    }
 
-String getCooling() {
-  return String(cooling);
-}
+    String getCooling() {
+      return String(cooling);
+    }
 
-String getSparking() {
-  return String(sparking);
-}
+    String getSparking() {
+      return String(sparking);
+    }
 
-String getSpeed() {
-  return String(speed);
-}
+    String getSpeed() {
+      return String(speed);
+    }
 
-String getTwinkleSpeed() {
-  return String(twinkleSpeed);
-}
+    String getTwinkleSpeed() {
+      return String(twinkleSpeed);
+    }
 
-String getTwinkleDensity() {
-  return String(twinkleDensity);
-}
+    String getTwinkleDensity() {
+      return String(twinkleDensity);
+    }
 
-String getCoolLikeIncandescent() {
-  return String(coolLikeIncandescent);
-}
+    String getCoolLikeIncandescent() {
+      return String(coolLikeIncandescent);
+    }
 
-String getName() {
-  return nameString;
-}
+    String getName() {
+      return nameString;
+    }
 
-String getUtcOffsetIndex() {
-  return String(utcOffsetIndex);
-}
+    String getUtcOffsetIndex() {
+      return String(utcOffsetIndex);
+    }
 
-String setUtcOffsetIndexString(String value)
-{
-  auto tmp = setUtcOffsetIndex(value.toInt()); 
-  return String(tmp);
-}
+    String setUtcOffsetIndexString(String value)
+    {
+      auto tmp = setUtcOffsetIndex(value.toInt()); 
+      return String(tmp);
+    }
 
-// Pride Playground fields
+    // Pride Playground fields
 
-String getSaturationBpm() {
-  return String(saturationBpm);
-}
-String setSaturationBpm(String value)
-{
-  saturationBpm = value.toInt();
-  return value;
-}
+    String getSaturationBpm() {
+      return String(saturationBpm);
+    }
+    String setSaturationBpm(String value)
+    {
+      saturationBpm = value.toInt();
+      return value;
+    }
 
-String getSaturationMin() {
-  return String(saturationMin);
-}
-String setSaturationMin(String value) {
-  saturationMin = value.toInt();
-  return value;
-}
+    String getSaturationMin() {
+      return String(saturationMin);
+    }
+    String setSaturationMin(String value) {
+      saturationMin = value.toInt();
+      return value;
+    }
 
-String getSaturationMax() {
-  return String(saturationMax);
-}
-String setSaturationMax(String value) {
-  saturationMax = value.toInt();
-  return value;
-}
+    String getSaturationMax() {
+      return String(saturationMax);
+    }
+    String setSaturationMax(String value) {
+      saturationMax = value.toInt();
+      return value;
+    }
 
-String getBrightDepthBpm() {
-  return String(brightDepthBpm);
-}
-String setBrightDepthBpm(String value) {
-  brightDepthBpm = value.toInt();
-  return value;
-}
+    String getBrightDepthBpm() {
+      return String(brightDepthBpm);
+    }
+    String setBrightDepthBpm(String value) {
+      brightDepthBpm = value.toInt();
+      return value;
+    }
 
-String getBrightDepthMin() {
-  return String(brightDepthMin);
-}
-String setBrightDepthMin(String value) {
-  brightDepthMin = value.toInt();
-  return value;
-}
+    String getBrightDepthMin() {
+      return String(brightDepthMin);
+    }
+    String setBrightDepthMin(String value) {
+      brightDepthMin = value.toInt();
+      return value;
+    }
 
-String getBrightDepthMax() {
-  return String(brightDepthMax);
-}
-String setBrightDepthMax(String value) {
-  brightDepthMax = value.toInt();
-  return value;
-}
+    String getBrightDepthMax() {
+      return String(brightDepthMax);
+    }
+    String setBrightDepthMax(String value) {
+      brightDepthMax = value.toInt();
+      return value;
+    }
 
-String getBrightThetaIncBpm() {
-  return String(brightThetaIncBpm);
-}
-String setBrightThetaIncBpm(String value) {
-  brightThetaIncBpm = value.toInt();
-  return value;
-}
+    String getBrightThetaIncBpm() {
+      return String(brightThetaIncBpm);
+    }
+    String setBrightThetaIncBpm(String value) {
+      brightThetaIncBpm = value.toInt();
+      return value;
+    }
 
-String getBrightThetaIncMin() {
-  return String(brightThetaIncMin);
-}
-String setBrightThetaIncMin(String value) {
-  brightThetaIncMin = value.toInt();
-  return value;
-}
+    String getBrightThetaIncMin() {
+      return String(brightThetaIncMin);
+    }
+    String setBrightThetaIncMin(String value) {
+      brightThetaIncMin = value.toInt();
+      return value;
+    }
 
-String getBrightThetaIncMax() {
-  return String(brightThetaIncMax);
-}
-String setBrightThetaIncMax(String value) {
-  brightThetaIncMax = value.toInt();
-  return value;
-}
+    String getBrightThetaIncMax() {
+      return String(brightThetaIncMax);
+    }
+    String setBrightThetaIncMax(String value) {
+      brightThetaIncMax = value.toInt();
+      return value;
+    }
 
-String getMsMultiplierBpm() {
-  return String(msMultiplierBpm);
-}
-String setMsMultiplierBpm(String value) {
-  msMultiplierBpm = value.toInt();
-  return value;
-}
+    String getMsMultiplierBpm() {
+      return String(msMultiplierBpm);
+    }
+    String setMsMultiplierBpm(String value) {
+      msMultiplierBpm = value.toInt();
+      return value;
+    }
 
-String getMsMultiplierMin() {
-  return String(msMultiplierMin);
-}
-String setMsMultiplierMin(String value) {
-  msMultiplierMin = value.toInt();
-  return value;
-}
+    String getMsMultiplierMin() {
+      return String(msMultiplierMin);
+    }
+    String setMsMultiplierMin(String value) {
+      msMultiplierMin = value.toInt();
+      return value;
+    }
 
-String getMsMultiplierMax() {
-  return String(msMultiplierMax);
-}
-String setMsMultiplierMax(String value) {
-  msMultiplierMax = value.toInt();
-  return value;
-}
+    String getMsMultiplierMax() {
+      return String(msMultiplierMax);
+    }
+    String setMsMultiplierMax(String value) {
+      msMultiplierMax = value.toInt();
+      return value;
+    }
 
-String getHueIncBpm() {
-  return String(hueIncBpm);
-}
-String setHueIncBpm(String value) {
-  hueIncBpm = value.toInt();
-  return value;
-}
+    String getHueIncBpm() {
+      return String(hueIncBpm);
+    }
+    String setHueIncBpm(String value) {
+      hueIncBpm = value.toInt();
+      return value;
+    }
 
-String getHueIncMin() {
-  return String(hueIncMin);
-}
-String setHueIncMin(String value) {
-  hueIncMin = value.toInt();
-  return value;
-}
+    String getHueIncMin() {
+      return String(hueIncMin);
+    }
+    String setHueIncMin(String value) {
+      hueIncMin = value.toInt();
+      return value;
+    }
 
-String getHueIncMax() {
-  return String(hueIncMax);
-}
-String setHueIncMax(String value) {
-  hueIncMax = value.toInt();
-  return value;
-}
+    String getHueIncMax() {
+      return String(hueIncMax);
+    }
+    String setHueIncMax(String value) {
+      hueIncMax = value.toInt();
+      return value;
+    }
 
-String getSHueBpm() {
-  return String(sHueBpm);
-}
-String setSHueBpm(String value) {
-  sHueBpm = value.toInt();
-  return value;
-}
+    String getSHueBpm() {
+      return String(sHueBpm);
+    }
+    String setSHueBpm(String value) {
+      sHueBpm = value.toInt();
+      return value;
+    }
 
-String getSHueMin() {
-  return String(sHueMin);
-}
-String setSHueMin(String value) {
-  sHueMin = value.toInt();
-  return value;
-}
+    String getSHueMin() {
+      return String(sHueMin);
+    }
+    String setSHueMin(String value) {
+      sHueMin = value.toInt();
+      return value;
+    }
 
-String getSHueMax() {
-  return String(sHueMax);
-}
-String setSHueMax(String value) {
-  sHueMax = value.toInt();
-  return value;
-}
+    String getSHueMax() {
+      return String(sHueMax);
+    }
+    String setSHueMax(String value) {
+      sHueMax = value.toInt();
+      return value;
+    }
 
   }
 
