eee5b0e142faffa19d512324f25c521c3b6df5b3
Jason Coon <3598755+jasoncoon@users.noreply.github.com>
2021-12-12 10:53:47 -0600

    Install arduino-cli libraries.

--- ci/build-arduino.sh
+++ ci/build-arduino.sh
@@ -22,4 +22,10 @@ arduino-cli core update-index --additional-urls https://arduino.esp8266.com/stab
 # Install Arduino esp8266 core
 arduino-cli core install esp8266:esp8266 --additional-urls https://arduino.esp8266.com/stable/package_esp8266com_index.json
 
+# Install libraries
+arduino-cli lib install ArduinoJson@6.18.5
+arduino-cli lib install FastLED@3.4.0
+arduino-cli lib install WiFiManager@2.0.4-beta
+arduino-cli lib install NTPClient@3.2.0
+
 arduino-cli compile --fqbn esp8266:esp8266:d1_mini ./esp8266-fastled-webserver/esp8266-fastled-webserver.ino
