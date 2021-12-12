cf274fa24d29d5c069b78a5f87d713b45c6a4c25
Jason Coon <3598755+jasoncoon@users.noreply.github.com>
2021-12-12 10:47:52 -0600

    Add url to arduino-cli core update-index command.

--- ci/build-arduino.sh
+++ ci/build-arduino.sh
@@ -17,9 +17,9 @@ mkdir $HOME/Arduino/libraries
 export PATH=$PATH:$GITHUB_WORKSPACE/bin
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
 arduino-cli config init
-arduino-cli core update-index
+arduino-cli core update-index --additional-urls https://arduino.esp8266.com/stable/package_esp8266com_index.json
 
 # Install Arduino esp8266 core
-arduino-cli core install "esp8266:esp8266" --additional-urls "https://arduino.esp8266.com/stable/package_esp8266com_index.json" -v
+arduino-cli core install esp8266:esp8266 --additional-urls https://arduino.esp8266.com/stable/package_esp8266com_index.json
 
 arduino-cli compile --fqbn esp8266:esp8266:d1_mini ./esp8266-fastled-webserver/esp8266-fastled-webserver.ino
