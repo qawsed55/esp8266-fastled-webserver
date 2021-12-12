78441f4b8a21d069ff4e89e2602028db8be65400
Jason Coon <3598755+jasoncoon@users.noreply.github.com>
2021-12-12 10:18:48 -0600

    Attempt at ci via github actions. Build with both arduino-cli and pio on each push & pr.

---
+++ ci/build-arduino.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Enable the globstar shell option
+shopt -s globstar
+
+# Make sure we are inside the github workspace
+cd $GITHUB_WORKSPACE
+
+# Create directories
+mkdir $HOME/Arduino
+mkdir $HOME/Arduino/libraries
+
+# Install Arduino IDE
+export PATH=$PATH:$GITHUB_WORKSPACE/bin
+curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
+arduino-cli config init
+arduino-cli core update-index
+
+# Install Arduino esp8266 core
+arduino-cli core install esp8266:esp8266
+
+arduino-cli compile --fqbn esp8266:esp8266:d1_mini ./esp8266-fastled-webserver/esp8266-fastled-webserver.ino
