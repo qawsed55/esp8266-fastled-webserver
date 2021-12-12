e536f417d87728c00d973e31c9ef53b3897913af
Jason Coon <3598755+jasoncoon@users.noreply.github.com>
2021-12-12 10:44:49 -0600

    Quote all the things?

--- ci/build-arduino.sh
+++ ci/build-arduino.sh
@@ -20,6 +20,6 @@ arduino-cli config init
 arduino-cli core update-index
 
 # Install Arduino esp8266 core
-arduino-cli core install "esp8266:esp8266" --additional-urls https://arduino.esp8266.com/stable/package_esp8266com_index.json -v
+arduino-cli core install "esp8266:esp8266" --additional-urls "https://arduino.esp8266.com/stable/package_esp8266com_index.json" -v
 
 arduino-cli compile --fqbn esp8266:esp8266:d1_mini ./esp8266-fastled-webserver/esp8266-fastled-webserver.ino
